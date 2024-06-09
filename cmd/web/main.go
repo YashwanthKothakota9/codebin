1ad7871b0543b4985b84fe10aabb6cb63b9af4b0
YashwanthK <yashwanthkothakota@gmail.com>
2024-06-09 20:59:44 +0530

    add file server to serve static files

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,15 +5,26 @@ import (
 	"net/http"
 )
 
-func main(){
+func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("GET /{$}",home)
-	mux.HandleFunc("GET /code/view/{id}",codeView)
-	mux.HandleFunc("GET /code/create",codeCreate)
-	mux.HandleFunc("POST /code/create",codeCreatePost)
+
+	// Create a file server which serves files out of the "./ui/static" directory.
+	// Note that the path given to the http.Dir function is relative to the project
+	// directory root.
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	// Use the mux.Handle() function to register the file server as the handler for
+	// all URL paths that start with "/static/". For matching paths, we strip the
+	// "/static" prefix before the request reaches the file server.
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
+	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("GET /code/view/{id}", codeView)
+	mux.HandleFunc("GET /code/create", codeCreate)
+	mux.HandleFunc("POST /code/create", codeCreatePost)
 
 	log.Println("Server running on PORT: 4000")
 
-	err := http.ListenAndServe(":4000",mux)
+	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
