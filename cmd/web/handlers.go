0d4b3d72205a57c72afb57e518872f7b41f968f8
YashwanthK <yashwanthkothakota@gmail.com>
2024-06-09 15:29:16 +0530

    add named templates

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -9,47 +9,51 @@ import (
 	//family of funcs for safely parsing and rendering HTML templates
 )
 
-func home(w http.ResponseWriter,r *http.Request){
-	w.Header().Add("Server","Go")
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Server", "Go")
 
-	// Use the template.ParseFiles() function to read the template file into a
-    // template set. If there's an error, we log the detailed error message, use
-    // the http.Error() function to send an Internal Server Error response to the
-    // user, and then return from the handler so no subsequent code is executed.
-	ts, err := template.ParseFiles("./ui/html/pages/home.tmpl")
-	if err!=nil{
+	// Initialize a slice containing the paths to the two files. It's important
+	// to note that the file containing our base template must be the *first*
+	// file in the slice.
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/pages/home.tmpl",
+	}
+
+	// Use the template.ParseFiles() function to read the files and store the
+	// templates in a template set. Notice that we use ... to pass the contents
+	// of the files slice as variadic arguments.
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
 		log.Print(err.Error())
-		http.Error(w,"Internal Server error",http.StatusInternalServerError)
+		http.Error(w, "Internal Server error", http.StatusInternalServerError)
 		return
 	}
 
-	// Then we use the Execute() method on the template set to write the
-    // template content as the response body. The last parameter to Execute()
-    // represents any dynamic data that we want to pass in, which for now we'll
-    // leave as nil.
-
-	err = ts.Execute(w,nil)
-	if err!=nil{
+	// Use the ExecuteTemplate() method to write the content of the "base"
+	// template as the response body.
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
 		log.Print(err.Error())
-		http.Error(w,"Internal server error",http.StatusInternalServerError)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
 	}
 }
 
-func codeView(w http.ResponseWriter,r *http.Request){
-	id,err := strconv.Atoi(r.PathValue("id"))
-	if err!=nil || id<1{
-		http.NotFound(w,r)
+func codeView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
 		return
 	}
 
-	fmt.Fprintf(w, "Display a specific code with ID %d...",id)
+	fmt.Fprintf(w, "Display a specific code with ID %d...", id)
 }
 
-func codeCreate(w http.ResponseWriter,r *http.Request){
+func codeCreate(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Displaying a form for creating new code snippet..."))
 }
 
-func codeCreatePost(w http.ResponseWriter,r *http.Request){
+func codeCreatePost(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte("Save a new code snippet..."))
 }
